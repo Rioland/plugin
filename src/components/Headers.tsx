6a7fd21f5a068f5df8cce21c08cb844566c7af45
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2025-03-21 02:26:04 -0700

    seller

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -177,7 +177,7 @@ const MyHeaders: React.FC<HeadersProps> = ({ currentPage }) => {
               >
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
               </svg>
-              <a href="" className="text-black fs-5 fw-5 me-3">
+              <a href="/become-seller" className="text-black fs-5 fw-5 me-3">
                 Become A seller
               </a>
               <a
