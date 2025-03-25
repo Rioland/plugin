238d99cf81ab292114b4386605a437010283113e
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2025-03-25 15:26:43 -0700

    jj

--- src/app/signup/register-buyer/page.tsx
+++ src/app/signup/register-buyer/page.tsx
@@ -113,7 +113,7 @@ export default function Home() {
                     required
                     value={value}
                     className="shadow-sm focus:ring-primary focus:border-primary block w-full px-4 py-4 rounded-md"
-                    onChange={(_) => setValue} />
+                    onChange={() => setValue} />
 
                   {/* <Input
                     type="tel"
