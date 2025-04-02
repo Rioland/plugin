abf9b45c2cbdc8b965ddd413c61b5505086411c7
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2025-04-02 03:31:07 -0700

    jkjk

--- src/app/dashboard/seller-dashboard/kyc/page.tsx
+++ src/app/dashboard/seller-dashboard/kyc/page.tsx
@@ -1,5 +1,5 @@
+
 'use client'
-// import { ApiBaseUrl } from '@/helper/functions';
 import React, { Fragment } from 'react'
 // import { toast, Toaster } from 'sonner';
 import Cookies from 'js-cookie';
@@ -14,12 +14,12 @@ export default function page() {
         const user = Cookies.get("currentUser") as string;
         const currentUser = user ? JSON.parse(user) : null;
         console.log(currentUser);
-        // const [kycCurrentPage, setkycCurrentPage] = useState("start");
+        const [kycCurrentPage, setkycCurrentPage] =React.useState("start");
 
 
         return (
                 <Fragment>
-                        {/* {kycCurrentPage == 'start' ? <StartKc onContinue={() => setkycCurrentPage('level2')} /> : kycCurrentPage == 'level2' ? <UpgradeToleveltwo onContinue={() => setkycCurrentPage('success')} /> : <SuccefullKycUpload />} */}
+                        {kycCurrentPage == 'start' ? <StartKc onContinue={() => setkycCurrentPage('level2')} /> : kycCurrentPage == 'level2' ? <UpgradeToleveltwo onContinue={() => setkycCurrentPage('success')} /> : <SuccefullKycUpload />}
 
                 </Fragment>
         )
