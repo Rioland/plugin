f260a55ca75d943f693833f9ee52c2a3fc8a99ae
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2025-04-02 03:02:52 -0700

    kyc

--- src/app/dashboard/seller-dashboard/kyc/page.tsx
+++ src/app/dashboard/seller-dashboard/kyc/page.tsx
@@ -13,12 +13,15 @@ export default function page() {
         const user = Cookies.get("currentUser") as string;
         const currentUser = user ? JSON.parse(user) : null;
         console.log(currentUser);
+        const [kycCurrentPage, setkycCurrentPage] = useState("start");
 
 
         return (
                 <Fragment>
+                        {kycCurrentPage=='start' ?<StartKc  onContinue={()=>setkycCurrentPage('level2')} /> :kycCurrentPage=='level2'?<UpgradeToleveltwo onContinue={()=>setkycCurrentPage('success')}  /> : <SuccefullKycUpload /> }
                         {/* <StartKc  /> */}
-                        <UpgradeToleveltwo />
+                        {/* <SuccefullKycUpload /> */}
+                        {/* <UpgradeToleveltwo /> */}
                 </Fragment>
         )
 }
@@ -49,7 +52,7 @@ const StartKc: React.FC<StartKcProps> = ({ onContinue }) => {
         </div>);
 }
 
-const UpgradeToleveltwo = () => {
+const UpgradeToleveltwo :React.FC<StartKcProps>= ({onContinue}) => {
         const [selectedId, setSelectedId] = useState('passport')
         const [image, setImage] = useState<File | null>(null);
         const [preview, setPreview] = useState<string | null>(null);
@@ -139,6 +142,7 @@ const UpgradeToleveltwo = () => {
                                                       <Button
                                                         type="submit"
                                                         // onClick={uploadImage}
+                                                        onClick={onContinue}
                                                         className="w-full bg-yellow-500 py-6 mt-10"
                                                       >
                                                         Proceed to Verification <i className="fal fa-arrow-right-long"></i>
@@ -149,4 +153,31 @@ const UpgradeToleveltwo = () => {
                         <Toaster position="top-center" />
                 </div>
         );
+}
+
+interface SuccefullKycUploadProps {
+        onContinue?: () => void
+}
+const SuccefullKycUpload: React.FC<SuccefullKycUploadProps> = ({ onContinue }) => {
+
+        return (<div>
+                <h1 className='text-center text-4xl  font-semibold  py-8 text-blue-950'>Begin your ID-Verification</h1>
+                <p className='text-center text-lg text-gray-600 md:w-1/2 mx-auto pb-12'>Verify your identity to participate in tokensale.</p>
+
+                <Card className='w-full md:w-1/2 md:mx-auto py-12 px-12'>
+                        <div className='w-fit border-2 border-green-500 p-8 rounded-full mx-auto'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="green" className="size-12">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+</svg>
+
+
+                        </div>
+                        <p className='text-center text-lg text-gray-600'>You have not submitted your necessary documents to verify your identity. In order to purchase our tokens, please verify your identity.</p>
+                        {/*  */}
+                        <Button className="w-fit bg-yellow-500 py-6 mt-10 mx-auto  " onClick={onContinue}>
+                                CBack to Profile
+                        </Button>
+                </Card>
+                <Toaster position="top-center" />
+        </div>);
 }
