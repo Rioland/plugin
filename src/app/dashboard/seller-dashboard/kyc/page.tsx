cdb0ec0f0dc55abd48a0856979b48eec6f5de613
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2025-04-02 03:26:22 -0700

    kkk

--- src/app/dashboard/seller-dashboard/kyc/page.tsx
+++ src/app/dashboard/seller-dashboard/kyc/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 // import { ApiBaseUrl } from '@/helper/functions';
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 // import { toast, Toaster } from 'sonner';
 import Cookies from 'js-cookie';
 // import { useDropzone } from 'react-dropzone';
@@ -9,12 +9,12 @@ import StartKc from './Components/StartKc';
 import UpgradeToleveltwo from './Components/UpgradeToleveltwo';
 
 export default function page() {
-        const [loading, setLoading] = useState(false);
+        // const [loading, setLoading] = useState(false);
         //   const [profile, setProfile] = useState(null);
         const user = Cookies.get("currentUser") as string;
         const currentUser = user ? JSON.parse(user) : null;
         console.log(currentUser);
-        const [kycCurrentPage, setkycCurrentPage] = useState("start");
+        // const [kycCurrentPage, setkycCurrentPage] = useState("start");
 
 
         return (
