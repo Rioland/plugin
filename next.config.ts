8f9639a3540e80e2416accc38791fdd293cb681c
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2025-05-27 15:05:25 -0700

    hhh

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,20 @@ const withFlowbiteReact = require("flowbite-react/plugin/nextjs");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**', // allows all image paths
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**", // allows all image paths
       },
     ],
   },
 };
 
 export default withFlowbiteReact(nextConfig);
-
-
 
 module.exports = withFlowbiteReact(nextConfig);
