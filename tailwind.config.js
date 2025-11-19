c55688bd3e36b5f1dd5c2d254d3cd16e0d148dc8
Adedamola Emmanuel Aladesanmi <adedamolarioland2222@gmail.com>
2025-11-19 10:15:40 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
